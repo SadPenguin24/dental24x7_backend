dc1727757d9adfec2da5402788b577fc1ea46f69
kevde <danhendrix.pro@gmail.com>
2025-04-03 22:21:52 +0800

    fixed ports and package json build

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { rateLimiterUsingThirdParty } from "./middleware/rateLimiter.middleware"
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
